8ca3bae6c55c765b319c2add2bee746397ca303f
Raphaelrosano <126472103+Raphaelrosano@users.noreply.github.com>
2023-04-24 10:39:16 -0300

    Update q4.go

--- q4/q4.go
+++ q4/q4.go
@@ -10,6 +10,74 @@ package q4
 //Caso a lista possua apenas um elemento, a função deve retornar 3.
 
 func ClassifyPrices(prices []int) (int, error) {
-	// Seu código aqui
+	package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func classifyPrices(prices []int) (int, error) {
+	n := len(prices)
+	if n == 0 {
+		return 0, errors.New("a lista está vazia")
+	}
+	if n == 1 {
+		return 3, nil
+	}
+	isIncreasing := true
+	isDecreasing := true
+	for i := 1; i < n; i++ {
+		if prices[i] > prices[i-1] {
+			isDecreasing = false
+		} else if prices[i] < prices[i-1] {
+			isIncreasing = false
+		}
+	}
+	if isIncreasing {
+		return 1, nil
+	} else if isDecreasing {
+		return 2, nil
+	} else {
+		return 3, nil
+	}
+}
+
+func main() {
+	prices1 := []int{5, 4, 3, 2, 1}
+	prices2 := []int{1, 2, 3, 4, 5}
+	prices3 := []int{3, 2, 4, 1, 5}
+	prices4 := []int{4}
+
+	result1, err1 := classifyPrices(prices1)
+	result2, err2 := classifyPrices(prices2)
+	result3, err3 := classifyPrices(prices3)
+	result4, err4 := classifyPrices(prices4)
+
+	if err1 != nil {
+		fmt.Println("Erro:", err1)
+	} else {
+		fmt.Println("Resultado 1:", result1)
+	}
+
+	if err2 != nil {
+		fmt.Println("Erro:", err2)
+	} else {
+		fmt.Println("Resultado 2:", result2)
+	}
+
+	if err3 != nil {
+		fmt.Println("Erro:", err3)
+	} else {
+		fmt.Println("Resultado 3:", result3)
+	}
+
+	if err4 != nil {
+		fmt.Println("Erro:", err4)
+	} else {
+		fmt.Println("Resultado 4:", result4)
+	}
+}
+
 	return 0, nil
 }
